0e5b5f3a4098ed9e3c151fa1c911f65e7c05e784
wjwj910 <wjwj910@gmail.com>
2024-11-26 12:05:34 +0900

    work green

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,6 +1,10 @@
 
 public class Calc {
     public static int run(String expr){
-        return 8;
+        String[] exprBits = expr.split(" \\+ ");
+        int num1 = Integer.parseInt(exprBits[0]);
+        int num2 = Integer.parseInt(exprBits[1]);
+
+        return num1 + num2;
     }
 }
